ec432dcc8732fa50f6c0a9dcd34bb5d5caa5f6b5
Alladi Krishnapriya <alladikrishnapriyaa@gmail.com>
2026-09-07 19:25:50 +0530

    added app2 file

---
+++ app2.py
@@ -0,0 +1,9 @@
+# Define two numbers using variables
+a = 10
+b = 3
+
+# Basic operations
+addition = a + b        # 13
+subtraction = a - b     # 7
+multiplication = a * b  # 30
+division = a / b        # 3.3333333333333335 (Always returns a decimal)
